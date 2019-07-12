d09d70162e2db16e0c168b97ef7834b07e4186de
Serafin Lichtenhahn <serafinlichtenhahn@wearewondrous.com>
2019-07-12 16:55:22 +0200

    docs: add settings page

---
+++ example/pages/Settings.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ story.content.title }}</h1>
+    <p>
+      {{ story.content.text }}
+    </p>
+  </div>
+</template>
+
+<script>
+import get from 'lodash/get'
+
+export default {
+  async asyncData({ app, route, error }) {
+    const lang = get(route, 'params.lang', 'en')
+    const query = `cdn/stories${
+      lang === 'en' ? route.path.replace('/en', '') : route.path
+    }`
+
+    try {
+      const { data } = await app.$storyapi.get(query)
+
+      return data
+    } catch (e) {
+      error({
+        statusCode: 404,
+        message: 'Page not found'
+      })
+    }
+  }
+}
+</script>
